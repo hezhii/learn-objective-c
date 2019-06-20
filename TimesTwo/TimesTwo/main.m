7e47e05ad56e95ade66c89c0045432b6fd66aa0d
biu <hezhou920@gmail.com>
2019-06-20 12:38:45 +0800

    对象和内存

---
+++ TimesTwo/TimesTwo/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by 何洲 on 2019/6/20.
+//  Copyright © 2019 何洲. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSDate *currentTime = [NSDate date];
+        NSLog(@"currentTime's value is %p", currentTime);
+        NSDate *startTime = currentTime;
+        
+        sleep(2);
+        
+        currentTime = [NSDate date];
+        NSLog(@"currentTime's value is now %p", currentTime);
+        NSLog(@"The address of the original object is %p", startTime);
+        
+        currentTime = nil;
+        NSLog(@"currentTime's value is %p", currentTime);
+    }
+    return 0;
+}
